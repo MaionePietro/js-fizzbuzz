75e65a32c72b0fe29b0b0cacc979a7e51a0927fb
MaionePietro <pietromaionespam@gmail.com>
2023-02-02 16:59:43 +0100

    FEAT innerHTML

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 let i=0;
-
+let stamp = document.querySelector('body')
 for(i ; i<=100 ; i++){
     if( (i% 3 == 0) && (i % 5 == 0) ){
         console.log('FizzBuzz');
+        stamp.innerHTML = ' <h1>FizzBuzz</h1>';
     }else if(i % 3 == 0){
         console.log('Fizz');
+        stamp.innerHTML = ' <h1>Fizz</h1>';
     }else if(i % 5 == 0){
         console.log('Buzz');
+        stamp.innerHTML = ' <h1>Buzz</h1>';
     }else {
         console.log(i);
+        stamp.innerHTML = i;
     }
 }
