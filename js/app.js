f8e2057a32d33e9805781f4aa48ed1625082a685
MaionePietro <pietromaionespam@gmail.com>
2023-02-02 17:08:31 +0100

    FIX innerHTML

--- js/app.js
+++ js/app.js
@@ -3,15 +3,15 @@ let stamp = document.querySelector('body')
 for(i ; i<=100 ; i++){
     if( (i% 3 == 0) && (i % 5 == 0) ){
         console.log('FizzBuzz');
-        stamp.innerHTML = ' <h1>FizzBuzz</h1>';
+        stamp.innerHTML += ' <h1>FizzBuzz</h1>';
     }else if(i % 3 == 0){
         console.log('Fizz');
-        stamp.innerHTML = ' <h1>Fizz</h1>';
+        stamp.innerHTML += ' <h1>Fizz</h1>';
     }else if(i % 5 == 0){
         console.log('Buzz');
-        stamp.innerHTML = ' <h1>Buzz</h1>';
+        stamp.innerHTML += ' <h1>Buzz</h1>';
     }else {
         console.log(i);
-        stamp.innerHTML = i;
+        stamp.innerHTML += '<h1></h1>';
     }
 }
